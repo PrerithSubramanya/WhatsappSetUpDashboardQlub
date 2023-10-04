4413f953535bd14e07c51787c011f0d288d1cc4e
Prerith Subramanya <prerithsubramanya@gmail.com>
2023-10-04 18:36:51 +1100

    added a button

--- main.py
+++ main.py
@@ -21,8 +21,9 @@ st.title("Whatsapp Notification Dashboard")
 
 if data:
     search_term = st.text_input("Search for a group by name:")
+    search_button = st.button("Search")
 
-    if search_term:
+    if search_button and search_term:
         # Using fuzzy matching to find the best match
         names = [item["name"] for item in data]
         best_match, score = process.extractOne(search_term, names)
